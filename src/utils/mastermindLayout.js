b4d39cc7fa03ec7cac1d3059ca52e76af8e4d704
danielalbertjeffery <daniel.albert.jeffery@gmail.com>
2017-08-11 07:34:00 +0800

    Changed mastermindLayout.js to use a looping function to create initial mastermindLayout rather than hard coding it

--- src/utils/mastermindLayout.js
+++ src/utils/mastermindLayout.js
@@ -1,42 +1,10 @@
-export default [
-	{
-		colored: ["white", "white", "white", "white"],
-		feedback: ["white", "white", "white", "white"]
-	},
-	{
-		colored: ["white", "white", "white", "white"],
-		feedback: ["white", "white", "white", "white"]
-	},
-	{
-		colored: ["white", "white", "white", "white"],
-		feedback: ["white", "white", "white", "white"]
-	},
-	{
-		colored: ["white", "white", "white", "white"],
-		feedback: ["white", "white", "white", "white"]
-	},
-	{
-		colored: ["white", "white", "white", "white"],
-		feedback: ["white", "white", "white", "white"]
-	},
-	{
-		colored: ["white", "white", "white", "white"],
-		feedback: ["white", "white", "white", "white"]
-	},
-	{
-		colored: ["white", "white", "white", "white"],
-		feedback: ["white", "white", "white", "white"]
-	},
-	{
-		colored: ["white", "white", "white", "white"],
-		feedback: ["white", "white", "white", "white"]
-	},
-	{
-		colored: ["white", "white", "white", "white"],
-		feedback: ["white", "white", "white", "white"]
-	},
-    {
-		colored: ["white", "white", "white", "white"],
-		feedback: ["white", "white", "white", "white"]
-	}
-];
+export default function createMastermindLayout() {
+    const rowLength = 10;
+    const mastermindLayout = [];
+    for (let i = 0; i < rowLength; i++) {
+        mastermindLayout[i] = {};
+        mastermindLayout[i].colored = ["white", "white", "white", "white"];
+        mastermindLayout[i].feedback = ["white", "white", "white", "white"];
+    }
+    return mastermindLayout;
+}

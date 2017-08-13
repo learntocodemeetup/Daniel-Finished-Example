9deb2f7d0ebfef0f47e4e4fe17f9ba48a862fbe7
danielalbertjeffery <daniel.albert.jeffery@gmail.com>
2017-08-13 15:07:39 +0800

    Changes to Mel's algorithm

--- public/mels-algorithm.js
+++ public/mels-algorithm.js
@@ -1,24 +1,19 @@
-var mastermindArray = ["red", "blue", "yellow", "orange"];
-
-var firstGuess = ["red", "yellow", "red", "indigo"];
-
-function feedback(mastermindArray, firstGuess) {
+function feedback(mastermindArray, guess) {
 
 	var precise = 0;
     var partial = 0;
 
-    var colourList = ["red", "blue", "yellow", "orange","indigo","green"];
-    var checkColour = [false, false,false,false,false,false];
+    var checkColour = [false, false,false,false];
 
-    for (let i = 0; i < firstGuess.length; i++) {
-        if (firstGuess[i] === mastermindArray[i]) {
+    for (let i = 0; i < guess.length; i++) {
+        if (guess[i] === mastermindArray[i]) {
             precise += 1;
         }
         for (let j=0; j< mastermindArray.length; j++){
 
-          if (firstGuess[i] === mastermindArray[j]) {
-              for(let k=0; k < colourList.length;k++){
-                  if(firstGuess[i] === colourList[k] && checkColour[k]==false){
+          if (guess[i] === mastermindArray[j]) {
+              for(let k=0; k < mastermindArray.length;k++){
+                  if(guess[i] === mastermindArray[k] && checkColour[k]==false){
                     checkColour[k]=true;
                     partial += 1;
                   }
@@ -27,7 +22,7 @@ function feedback(mastermindArray, firstGuess) {
           }
         }
     }
-    partial = partial - precise;
+    partial = Math.max(partial - precise,0);
 
     console.log(`There were ${precise} precise`);
     console.log(`There were ${partial} partial`);
@@ -46,7 +41,5 @@ function feedback(mastermindArray, firstGuess) {
 
     console.log("feedback", feedbackArray);
 
+    return feedbackArray;
 }
-
-
-// this.setState({feedbackArray: feedbackArray});
